d219f5ac0183624a7bb4745bbc206c7eb25976ca
Miqueias <miqueias.caetano@doctum.edu.br>
2019-04-12 18:15:07 -0300

    ok

--- www/js/onesignal.js
+++ www/js/onesignal.js
@@ -1,10 +1,13 @@
 document.addEventListener('deviceready', function () {
   // Enable to debug issues.
   // window.plugins.OneSignal.setLogLevel({logLevel: 4, visualLevel: 4});
-  // OneSignal.getUserId(function(id){alert(id)});
+  // window.plugins.OneSignal.getUserId(function(id){alert(id)});
   var notificationOpenedCallback = function(jsonData) {
     //alert('Funcionou!!!!');
+    	window.plugins.OneSignal.getUserId(function(id){alert(id)});
+
   };
+	window.plugins.OneSignal.getUserId(function(id){alert(id)});
 
   window.plugins.OneSignal
     .startInit('4eafd905-0ac3-40d3-816a-c8124086a4e1')
