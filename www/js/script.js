ad66562dea256b6ea0361cbc6af4d464c72f486c
miqueias91 <miqueiasmcaetano@gmail.com>
2019-04-12 03:05:41 -0300

    ok

--- www/js/script.js
+++ www/js/script.js
@@ -1,26 +1,3 @@
-// Add to index.js or the first page that loads with your app.
-// For Intel XDK and please add this to your app.js.
-
-/*document.addEventListener('deviceready', function () {
-	console.log('aqui')
-
-  // Enable to debug issues.
-  // window.plugins.OneSignal.setLogLevel({logLevel: 4, visualLevel: 4});
-  
-  var notificationOpenedCallback = function(jsonData) {
-    console.log('notificationOpenedCallback: ' + JSON.stringify(jsonData));
-  };
-
-  window.plugins.OneSignal
-    .startInit("4eafd905-0ac3-40d3-816a-c8124086a4e1")
-    .handleNotificationOpened(notificationOpenedCallback)
-    .endInit();
-
-window.plugins.OneSignal.promptForPushNotificationsWithUserResponse(function(accepted) {
-  console.log("User accepted notifications: " + accepted);
-});
-}, false);*/
-
 var clima = {ec:'Encoberto com Chuvas Isoladas',ci:'Chuvas Isoladas',c:'Chuva',in:'Instável',pp:'Poss. de Pancadas de Chuva',cm:'Chuva pela Manhã',cn:'Chuva a Noite',pt:'Pancadas de Chuva a Tarde',pm:'Pancadas de Chuva pela Manhã',np:'Nublado e Pancadas de Chuva',pc:'Pancadas de Chuva',pn:'Parcialmente Nublado',cv:'Chuvisco',ch:'Chuvoso',t:'Tempestade',ps:'Predomínio de Sol',e:'Encoberto',n:'Nublado',cl:'Céu Claro',nv:'Nevoeiro',g:'Geada',ne:'Neve',nd:'Não Definido',pnt:'Pancadas de Chuva a Noite',psc:'Possibilidade de Chuva',pcm:'Possibilidade de Chuva pela Manhã',pct:'Possibilidade de Chuva a Tarde',pcn:'Possibilidade de Chuva a Noite',npt:'Nublado com Pancadas a Tarde',npn:'Nublado com Pancadas a Noite',ncn:'Nublado com Poss. de Chuva a Noite',nct:'Nublado com Poss. de Chuva a Tarde',ncm:'Nubl. c/ Poss. de Chuva pela Manhã',npm:'Nublado com Pancadas pela Manhã',npp:'Nublado com Possibilidade de Chuva',vn:'Variação de Nebulosidade',ct:'Chuva a Tarde',ppn:'Poss. de Panc. de Chuva a Noite',ppt:'Poss. de Panc. de Chuva a Tarde',ppm:'Poss. de Panc. de Chuva pela Manhã'};
 var semana = ["Dom", "Seg", "Ter", "Qua", "Qui", "Sex", "Sáb"];
 
