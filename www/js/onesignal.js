54fce0e75892625915181a61025f5f4121f89ca6
miqueias91 <miqueiasmcaetano@gmail.com>
2019-04-12 03:07:14 -0300

    ok

--- www/js/onesignal.js
+++ www/js/onesignal.js
@@ -1,14 +1,15 @@
 document.addEventListener('deviceready', function () {
-        // Enable to debug issues.
-        // window.plugins.OneSignal.setLogLevel({logLevel: 4, visualLevel: 4});
-        
-        var notificationOpenedCallback = function(jsonData) {
-           
-           alert('notificationOpenedCallback: ' + JSON.stringify(jsonData));
-        };
+  // Enable to debug issues.
+  // window.plugins.OneSignal.setLogLevel({logLevel: 4, visualLevel: 4});
+  
+  var notificationOpenedCallback = function(jsonData) {
+     
+     //alert('notificationOpenedCallback: ' + JSON.stringify(jsonData));
+     alert('Funcionou!!!!');
+  };
 
-        window.plugins.OneSignal
-          .startInit('4eafd905-0ac3-40d3-816a-c8124086a4e1')
-          .handleNotificationOpened(notificationOpenedCallback  )
-          .endInit();
-      }, false);
+  window.plugins.OneSignal
+    .startInit('4eafd905-0ac3-40d3-816a-c8124086a4e1')
+    .handleNotificationOpened(notificationOpenedCallback  )
+    .endInit();
+}, false);
