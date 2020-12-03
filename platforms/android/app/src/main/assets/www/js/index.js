f2876bf1f39c22c0a87949b407bf87dcfc00ccc8
Miqueias Aspire-E1-571 <miqueiasmcaetano@gmail.com>
2020-12-03 14:53:35 -0300

    ok

--- platforms/android/app/src/main/assets/www/js/index.js
+++ platforms/android/app/src/main/assets/www/js/index.js
@@ -419,7 +419,7 @@ var app = {
 
           for(var i = start; i <=  end; i++){
             if (myBook.chapters[obj_v.chapter - 1][i]) {
-                obj_v.text += '<ons-list-item>'+
+                obj_v.text += '<ons-list-item onclick="fn.pushPage({\'id\': \'textoLivro.html\', \'title\': \''+myBook.abbrev+'||'+myBook.name+'||'+myBook.chapters.length+'||'+(parseInt(capitulo))+'\'});">'+
                   '<p style="font-size: 20px;line-height:30px;text-align:justify">'+
                     myBook.chapters[obj_v.chapter - 1][i] +
                   '</p>'+
